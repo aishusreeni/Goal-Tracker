417e5dc55763d752dde2edf95235ef8c0c30fe35
Aishwarya <aishusreeni@gmail.com>
2017-10-28 17:50:35 -0700

    Add the activities

--- app/views.py
+++ app/views.py
@@ -1,6 +1,45 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    user = {'nickname': 'Aishwarya!'}  # fake user
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
+
+@app.route('/food')
+def food():
+    user = {'nickname': 'Aishwarya!'}
+    return render_template('food.html',
+                           title='Home',
+                           user=user)
+
+@app.route('/exercise')
+def exercise():
+    user = {'nickname': 'Aishwarya!'}
+    return render_template('exercise.html',
+                           title='Home',
+                           user=user)
+
+@app.route('/skin')
+def skin():
+    user = {'nickname': 'Aishwarya!'}
+    return render_template('skin.html',
+                           title='Home',
+                           user=user)
+
+@app.route('/health')
+def health():
+    user = {'nickname': 'Aishwarya!'}
+    return render_template('health.html',
+                           title='Home',
+                           user=user)
+
+@app.route('/read')
+def read():
+    user = {'nickname': 'Aishwarya!'}
+    return render_template('read.html',
+                           title='Home',
+                           user=user)
